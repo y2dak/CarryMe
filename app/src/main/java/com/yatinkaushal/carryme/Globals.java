7e1fa4bc05185ee847c3803289f2957b3c2d9978
Yatin Kaushal <y2dak@users.noreply.github.com>
2017-02-01 11:34:01 -0500

    Added Google Sign In (#1)
    * Initial commit
    * Added Google Sign In

---
+++ app/src/main/java/com/yatinkaushal/carryme/Globals.java
@@ -0,0 +1,10 @@
+package com.yatinkaushal.carryme;
+
+/**
+ * Created by yatinkaushal on 2/1/17.
+ */
+
+public class Globals {
+    public static final String packagePath = "com.yatinkaushal.carryme";
+    public static String loggedIn = "loggedIn";
+}
